cfbf2e75547e9167506bb45bec1165f902664e7d
Kalansooriya S. H <it20137700@my.sliit.lk>
2023-09-24 08:28:07 +0530

    xss vulnerability fixed

--- Vulnerebilities_Fixed_App/RPMT-frontend/src/components/sug_resdoc_feedback/sug_resdoc_feedback.js
+++ Vulnerebilities_Fixed_App/RPMT-frontend/src/components/sug_resdoc_feedback/sug_resdoc_feedback.js
@@ -2,6 +2,8 @@ import { Button } from '@mui/material';
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import swal from 'sweetalert';
+import DOMPurify from 'dompurify';
+
 
 const Sug_resdoc_feedback= (props) => {
   const history = useNavigate();
@@ -10,7 +12,7 @@ const Sug_resdoc_feedback= (props) => {
  
     const deleteHandler = async () => {
     
-        
+     
 
         swal({
           title: "Are you sure?",
@@ -38,8 +40,8 @@ const Sug_resdoc_feedback= (props) => {
           }
         });
     };
-   
- 
+    const url ="javascript: alert('XSS attack!')"
+    
 
   return (
     <div >
@@ -47,13 +49,15 @@ const Sug_resdoc_feedback= (props) => {
        <td width={"200px"}>{ResDocFileGroupId}</td>
        <td width={"200px"} >{ResDocFileSupervisor}</td>
        <td width={"200px"}>{ResDocFileTopic}</td>
-       <td width={"200px"}>{Feedback}</td>
+       <td width={"200px"} dangerouslySetInnerHTML={{ __html: Feedback }}></td>
+     
        <td width={"200px"}>{EvaluvatedDate}</td>
        <td width={"200px"}> 
        <a><button class="btn btn-danger" onClick={deleteHandler} ><i class="fa fa-trash-o" aria-hidden="true"></i>delete</button></a> 
         &nbsp;
-        <a href={`/AllResDocFeedback/${_id}`}><button class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> update </button></a> </td>
-      
+        
+        {/* <a href={DOMPurify.sanitize(_id).replace('javascript:', '')}><button class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> update </button></a></td> */}
+<a href={_id}><button class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> update </button></a> </td>
        </tr>
  
         
